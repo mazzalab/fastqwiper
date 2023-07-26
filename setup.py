2d64d4914eccd48da6b6173f6ceb586bade9b8bb
mazzalab <t.mazza@css-mendel.it>
2023-07-26 17:19:21 +0200

    [build] fix appveyor config file

--- setup.py
+++ setup.py
@@ -54,8 +54,7 @@ setup(
     install_requires=[
         "setuptools",
         "colorama==0.4.6",
-        "click==8.1.3",
-        "tqdm==4.38.0"
+        "click==8.1.3"
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -71,5 +70,5 @@ setup(
         'Developmental plan': 'https://github.com/mazzalab/fastqwiper/projects',
     },
     keywords='genomics, ngs, fastq, bioinformatics',
-    python_requires='3.8',
+    python_requires='==3.8',
 )
