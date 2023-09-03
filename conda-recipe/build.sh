3b3e79da362b67f5f014ecdc0bcd5ca892480144
mazzalab <t.mazza@css-mendel.it>
2023-09-03 20:55:39 +0200

    pre-release - attempt 3

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-$PYTHON setup.py install --single-version-externally-managed --record=$RECIPE_DIR/record.txt
+$PYTHON setup.py --ver ${{ vars.FASTQWIPER_VER }}${{ github.run_number }} install --single-version-externally-managed --record=$RECIPE_DIR/record.txt
