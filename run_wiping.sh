637548dccb8048f92b9f4952bb879c703e9aefcf
mazzalab <t.mazza@css-mendel.it>
2023-10-15 11:34:27 +0200

    Snakemake help

--- run_wiping.sh
+++ run_wiping.sh
@@ -35,5 +35,6 @@ then
     snakemake --config sample_name=$sample_name -s pipeline/fix_wipe_single_reads_sequential.smk --use-conda --cores $cores
   fi
 else
-  snakemake -n -r --config sample_name=$sample_name -s pipeline/fix_wipe_pairs_reads_sequential.smk --use-conda --cores $cores
+  echo "Snakemake help"
+  snakemake --help
 fi
