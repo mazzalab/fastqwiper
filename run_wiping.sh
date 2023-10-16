3845c3b3947b0eae4effa2d44f3484ade134cc24
mazzalab <t.mazza@css-mendel.it>
2023-10-16 20:43:37 +0200

    Fixing Singularity build

--- run_wiping.sh
+++ run_wiping.sh
@@ -5,34 +5,28 @@ cores=$(($2))
 sample_name=$3
 chunk_size=$(($4))
 
-echo "PASSED:"
-echo $mode
-echo $cores
-echo $sample_name
-echo $chunk_size
-
+# Enter the FastqWiper folder
 cd /fastqwiper
-ls -l
 
 if [ $mode == "paired" ]
 then
   if [ $cores -gt 1 ]
   then
     echo "Processing paired-end files in parallel"
-    snakemake --config sample_name=$sample_name chunk_size=$chunk_size -s pipeline/fix_wipe_pairs_reads_parallel.smk --use-conda --cores $cores
+    snakemake --config sample_name=$sample_name chunk_size=$chunk_size -s ./pipeline/fix_wipe_pairs_reads_parallel.smk --use-conda --cores $cores
   else
     echo "Processing paired-end files sequentially"
-    snakemake --config sample_name=$sample_name -s pipeline/fix_wipe_pairs_reads_sequential.smk --use-conda --cores $cores
+    snakemake --config sample_name=$sample_name -s ./pipeline/fix_wipe_pairs_reads_sequential.smk --use-conda --cores $cores
   fi
 elif [ $mode == "single" ]
 then
   if [ $cores -gt 1 ]
   then
     echo "Processing single-end file in parallel"
-    snakemake --config sample_name=$sample_name chunk_size=$chunk_size -s pipeline/fix_wipe_single_reads_parallel.smk --use-conda --cores $cores
+    snakemake --config sample_name=$sample_name chunk_size=$chunk_size -s ./pipeline/fix_wipe_single_reads_parallel.smk --use-conda --cores $cores
   else
     echo "Processing single-end file sequentially"
-    snakemake --config sample_name=$sample_name -s pipeline/fix_wipe_single_reads_sequential.smk --use-conda --cores $cores
+    snakemake --config sample_name=$sample_name -s ./pipeline/fix_wipe_single_reads_sequential.smk --use-conda --cores $cores
   fi
 else
   echo "Snakemake help"
