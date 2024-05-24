84a976c86464bcdbe196866002472e615ce1ac4f
mazzalab <t.mazza@css-mendel.it>
2024-05-24 15:13:42 +0200

    Fixed wipe_fastq

--- fastq_wiper/wiper.py
+++ fastq_wiper/wiper.py
@@ -23,11 +23,7 @@ bad_qual: int = 0
 qual_out_of_range: int = 0
 clean_reads: int = 0
 seq_len_neq_qual_len: int = 0
-orphan_lines: int = 0
 # endregion
-
-# Allowed character of the SEQ line
-reg = re.compile("^[ACGTN]+$")
 
 
 def open_fastq_file(file_path: str):
@@ -53,11 +49,10 @@ def read_next_line(fin, log_frequency: int):
     global tot_lines
 
     line = fin.readline()
-    # increment the total number of read not-empty lines
-    tot_lines += 1
-
-    line = skip_empty_lines(line, fin)
-    print_log_during_reading(tot_lines, log_frequency)
+    if line:
+        # increment the total number of read not-empty lines
+        tot_lines += 1
+        print_log_during_reading(tot_lines, log_frequency)
 
     return line
 
@@ -84,7 +79,7 @@ def check_header_line(line: str) -> str:
     header: str = ""
 
     if line.isprintable() and line.rfind("@") == 0:
-        header = line.rstrip()
+        header = line
     elif not line.isprintable():
         # This is a not printable header
         bad_header += 1
@@ -92,7 +87,7 @@ def check_header_line(line: str) -> str:
         # This is an uncompliant header line
         bad_header += 1
     else:  # elif header.rfind("@") > 0:
-        header = line.rstrip()
+        header = line
         header = header[header.rfind("@"):]
         fixed_header += 1
 
@@ -145,20 +140,6 @@ def check_qual_line(line: str) -> str:
     return qual
 
 
-def skip_empty_lines(line: str, fin) -> str:
-    global blank
-
-    while True:
-        if line:
-            line = line.rstrip()
-            break
-        else:
-            blank += 1
-            line = fin.readline()
-
-    return line
-
-
 def print_log_during_reading(lines, log_frequency):
     if lines % log_frequency == 0:
         logging.info(f"Cleaned {lines} reads")
@@ -173,7 +154,7 @@ def print_to_file(header, raw_seq, head_qual_sep, qual, fout):
 
 def print_log_to_file(log_out):
     global tot_lines, clean_reads, seq_len_neq_qual_len, bad_qual, qual_out_of_range, bad_plus, \
-        bad_seq, bad_header, fixed_header, fixed_plus, blank, orphan_lines
+        bad_seq, bad_header, fixed_header, fixed_plus, blank
 
     # Open file out summary log
     flog = open(log_out, "wt", encoding="utf-8")
@@ -192,14 +173,13 @@ def print_log_to_file(log_out):
     flog.write(f"QUAL out of range lines: {qual_out_of_range}/{tot_lines}" + "\n")
     flog.write(f"Len(SEQ) neq Len(QUAL): {seq_len_neq_qual_len}/{tot_lines}" + "\n")
     flog.write(f"Blank lines: {blank}/{tot_lines}" + "\n")
-    flog.write(f"Orphan lines: {orphan_lines}/{tot_lines}" + "\n")
 
     flog.close()
 
 
 def print_log_to_screen():
     global tot_lines, clean_reads, seq_len_neq_qual_len, bad_qual, qual_out_of_range, bad_plus, \
-        bad_seq, bad_header, fixed_header, fixed_plus, blank, orphan_lines
+        bad_seq, bad_header, fixed_header, fixed_plus, blank
 
     if tot_lines == (clean_reads*4):
         logging.info(f"Clean lines: {clean_reads * 4}/{tot_lines} ({round((clean_reads * 4 / tot_lines) * 100, 2)}%)")
@@ -252,162 +232,6 @@ def print_log_to_screen():
     else:
         logging.warning(f"Blank lines: {blank}/{tot_lines}")
 
-    if orphan_lines == 0:
-        logging.info(f"Orphan lines: {orphan_lines}/{tot_lines}")
-    else:
-        logging.warning(f"Orphan lines: {orphan_lines}/{tot_lines}")
-
-
-def count_remainder():
-    global tot_lines
-    return tot_lines % 4
-
-
-def wipe_fastq_old(fastq_in: str, fastq_out: str, log_out: str, log_frequency: int):
-    # MIN_HEADER_LEN = 10
-
-    fin = open_fastq_file(fastq_in)
-    if not fin:
-        logging.critical("The input FASTQ file does not exist or bad extension (.gz or .fastq.gz)")
-    else:
-        logging.info(f"Start wiping {fastq_in}")
-
-        # Open file out stream
-        fout = write_fastq_file(fastq_out)
-
-        # change if a lines is parsed successfully
-        checkpoint_flags = {
-            "at": False,
-            "seq": False,
-            "plus": False,
-            "qual": False,
-        }
-
-        # Cleaned lines to be printed
-        header: str = ""
-        raw_seq: str = ""
-        head_qual_sep: str = ""
-        qual: str = ""
-
-        # global variables anchor
-        global tot_lines, clean_reads, seq_len_neq_qual_len, head_print, bad_seq, plus_row, qual_odd_chars, unexpected_line
-
-        # Loop through 4-line reads
-        for line in fin:
-            tot_lines += 1  # increment the total number of read lines
-
-            print_log_during_reading(tot_lines, log_frequency)
-            line = skip_empty_lines(line, fin)
-
-            # header line
-            if (
-                line.isprintable()
-                and "@" in line
-                and not checkpoint_flags["at"]
-                and not checkpoint_flags["seq"]
-                and not checkpoint_flags["plus"]
-                and not checkpoint_flags["qual"]
-            ):
-                header = fix_header_line(line, checkpoint_flags)
-
-            # seq line
-            elif (
-                line.isprintable()
-                and checkpoint_flags["at"]
-                and not checkpoint_flags["seq"]
-                and not checkpoint_flags["plus"]
-                and not checkpoint_flags["qual"]
-            ):
-                raw_seq = fix_seq_line(line, checkpoint_flags)
-
-            # + line
-            elif (
-                "+" in line
-                and line.isprintable()
-                and checkpoint_flags["at"]
-                and checkpoint_flags["seq"]
-                and not checkpoint_flags["plus"]
-                and not checkpoint_flags["qual"]
-            ):
-                head_qual_sep = check_plus_line(line, checkpoint_flags)
-
-            # qual line
-            elif (
-                line.isprintable()
-                and checkpoint_flags["at"]
-                and checkpoint_flags["seq"]
-                and checkpoint_flags["plus"]
-                and not checkpoint_flags["qual"]
-            ):
-                qual = check_qual_line(line, checkpoint_flags)
-
-                # Eventually print to file
-                if len(raw_seq) == len(qual) and len(qual) != 0:
-                    print_to_file(header, raw_seq, head_qual_sep, qual, fout)
-                    clean_reads += 1
-                else:
-                    seq_len_neq_qual_len += 1
-
-                reset_flags(checkpoint_flags)
-
-            # unexpected line
-            else:
-                if (
-                    "@" in line
-                    and not checkpoint_flags["at"]
-                    and not checkpoint_flags["seq"]
-                    and not checkpoint_flags["plus"]
-                    and not checkpoint_flags["qual"]
-                ):
-                    head_print += 1
-                elif (
-                    "@" not in line
-                    and not checkpoint_flags["at"]
-                    and not checkpoint_flags["seq"]
-                    and not checkpoint_flags["plus"]
-                    and not checkpoint_flags["qual"]
-                ):
-                    unexpected_line += 1
-                elif (
-                    checkpoint_flags["at"]
-                    and not checkpoint_flags["seq"]
-                    and not checkpoint_flags["plus"]
-                    and not checkpoint_flags["qual"]
-                ):
-                    bad_seq += 1
-                elif (
-                    checkpoint_flags["at"]
-                    and checkpoint_flags["seq"]
-                    and not checkpoint_flags["plus"]
-                    and not checkpoint_flags["qual"]
-                ):
-                    plus_row += 1
-                elif (
-                    checkpoint_flags["at"]
-                    and checkpoint_flags["seq"]
-                    and checkpoint_flags["plus"]
-                    and not checkpoint_flags["qual"]
-                ):
-                    qual_odd_chars += 1
-                else:
-                    unexpected_line += 1
-
-                reset_flags(checkpoint_flags)
-
-        # All lines that do not refer to any header at the end of the file are considered "unexpected" (1 to 3 lines max)
-        unexpected_line += count_remainder()
-
-        fout.close()
-        fin.close()
-
-        # Print short report
-        logging.info("Successfully terminated\n")
-
-        if log_out:
-            print_log_to_file(log_out)
-        else:
-            print_log_to_screen()
-
 
 def wipe_fastq(fastq_in: str, fastq_out: str, log_out: str, log_frequency: int):
     # MIN_HEADER_LEN = 10
@@ -422,34 +246,37 @@ def wipe_fastq(fastq_in: str, fastq_out: str, log_out: str, log_frequency: int):
         fout = write_fastq_file(fastq_out)
 
         # global variables anchor
-        global tot_lines, clean_reads, seq_len_neq_qual_len, orphan_lines
+        global tot_lines, clean_reads, seq_len_neq_qual_len
 
         # Loop through 4-line reads
         for line in fin:
+            if not line.strip():
+                # skip empty lines
+                continue
+
             tot_lines += 1
             print_log_during_reading(tot_lines, log_frequency)
-            line = skip_empty_lines(line, fin)
 
             # PROCESS THE HEADER LINE
-            header: str = check_header_line(line)
+            header: str = check_header_line(line.rstrip())
             if not header:
                 continue
 
             # PROCESS THE SEQ LINE
             line = read_next_line(fin, log_frequency)
-            raw_seq: str = check_seq_line(line)
+            raw_seq: str = check_seq_line(line.rstrip())
             if not raw_seq:
                 continue
 
             # PROCESS the + line
             line = read_next_line(fin, log_frequency)
-            plus: str = check_plus_line(line)
+            plus: str = check_plus_line(line.rstrip())
             if not plus:
                 continue
 
             # PROCESS the QUAL line
             line = read_next_line(fin, log_frequency)
-            qual: str = check_qual_line(line)
+            qual: str = check_qual_line(line.rstrip())
             if not qual:
                 continue
 
@@ -459,9 +286,6 @@ def wipe_fastq(fastq_in: str, fastq_out: str, log_out: str, log_frequency: int):
                 clean_reads += 1
             else:
                 seq_len_neq_qual_len += 1
-
-        # All lines that do not refer to any read at the end of the file are considered "unexpected" (1 to 3 lines max)
-        orphan_lines = count_remainder()
 
         fout.close()
         fin.close()
@@ -480,7 +304,12 @@ if __name__ == "__main__":
     parser.add_argument("-i", '--fastq_in', help='The corrupted FASTQ file', required=True)
     parser.add_argument("-o", '--fastq_out', help='The wiped FASTQ file', required=True)
     parser.add_argument("-l", '--log_out', nargs='?', help='The file name of the final quality report summary')
-    parser.add_argument("-f", '--log_frequency', type=int, nargs='?', const=500000, help='The number of reads you want to print a status message')
+    parser.add_argument("-f", '--log_frequency', type=int, nargs='?', default=500000, const=500000, help='The number of reads you want to print a status message')
+    parser.add_argument("-a", '--alphabet', type=str, nargs='?', default="ACGTN", const="ACGTN",
+                        help='Allowed character in the SEQ line. Default: ACGTN')
     args = parser.parse_args()
+
+    # Allowed character of the SEQ line
+    reg = re.compile(f"^[{args.alphabet}]+$")
 
     wipe_fastq(args.fastq_in, args.fastq_out,args.log_out, args.log_frequency)
