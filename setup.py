2be6094030b2cb61e1b0e28a82417226c4aa18d7
irongraft <mazza.tommaso@gmail.com>
2024-11-20 10:08:22 +0100

    fix required python version >=3.10 and license

--- setup.py
+++ setup.py
@@ -28,5 +28,5 @@ setup(
         "License :: OSI Approved :: Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.10',
 )
